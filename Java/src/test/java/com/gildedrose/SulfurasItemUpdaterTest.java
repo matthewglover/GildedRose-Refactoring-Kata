c3d98337fbaa7ad58f1d412076216c976e350f08
Elle Hallal <daniellehallal@gmail.com>
2019-07-01 14:23:48 +0100

    Create SulfurasItemUpdater class

---
+++ Java/src/test/java/com/gildedrose/SulfurasItemUpdaterTest.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SulfurasItemUpdaterTest {
+    @Test
+    public void sulfurasSellInAndQualityNeverChange() {
+        Item sulfuras = new Item("Sulfuras, Hand of Ragnaros", 5, 40);
+
+        new SulfurasItemUpdater().updateItem(sulfuras);
+
+        assertEquals(5, sulfuras.sellIn);
+        assertEquals(40, sulfuras.quality);
+    }
+}
