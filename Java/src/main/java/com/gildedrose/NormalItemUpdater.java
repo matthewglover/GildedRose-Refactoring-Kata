23a6a9d0111b0e08026385a94e788f9391d09146
Elle Hallal <daniellehallal@gmail.com>
2019-07-01 11:42:29 +0100

    ItemUpdater - implement decreaseSellIn. Create NormalItemUpdater class

---
+++ Java/src/main/java/com/gildedrose/NormalItemUpdater.java
@@ -0,0 +1,26 @@
+package com.gildedrose;
+
+public class NormalItemUpdater extends ItemUpdater {
+
+    @Override
+    void setQualityForValidItem(Item item) {
+        if (isQualityMoreThanZero(item)) {
+            decreaseQualityByOne(item);
+        }
+    }
+
+    @Override
+    void setQualityForExpiredItem(Item item) {
+        if (isQualityMoreThanZero(item)) {
+            decreaseQualityByOne(item);
+        }
+    }
+
+    private boolean isQualityMoreThanZero(Item item) {
+        return item.quality > 0;
+    }
+
+    private void decreaseQualityByOne(Item item) {
+        item.quality -= 1;
+    }
+}
