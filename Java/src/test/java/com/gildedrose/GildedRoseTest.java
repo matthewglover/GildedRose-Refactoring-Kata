944a414104eb9e4f83e6188b28127eeee6e9a849
Elle Hallal <daniellehallal@gmail.com>
2019-07-01 14:34:42 +0100

    Add tests for conjured items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -129,4 +129,28 @@ public class GildedRoseTest {
 
         assertEquals(0, backstagePass.quality);
     }
+
+    @Test
+    public void qualityOfAConjuredItemDecreasesByTwo() {
+        Item conjuredItem = new Item("Conjured", 1, 10);
+        Item[] items = new Item[] {conjuredItem};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(0, conjuredItem.sellIn);
+        assertEquals(8, conjuredItem.quality);
+    }
+
+    @Test
+    public void whenTheSellByDateHasPassedTheQualityOfAConjuredItemDecreasesByFour() {
+        Item conjuredItem = new Item("Conjured", 0, 10);
+        Item[] items = new Item[] {conjuredItem};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(-1, conjuredItem.sellIn);
+        assertEquals(6, conjuredItem.quality);
+    }
 }
