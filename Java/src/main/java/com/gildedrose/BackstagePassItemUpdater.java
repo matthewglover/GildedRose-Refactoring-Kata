951d9a7f98e5778b98688ff6e9d6a4e025066cb0
Matt Glover <mglover@8thlight.com>
2019-07-05 20:56:28 +0100

    Refactor BackstagePassItemUpdater (simplify logic)

--- Java/src/main/java/com/gildedrose/BackstagePassItemUpdater.java
+++ Java/src/main/java/com/gildedrose/BackstagePassItemUpdater.java
@@ -1,12 +1,6 @@
 package com.gildedrose;
 
 class BackstagePassItemUpdater extends ItemUpdater {
-    private static final int EXPIRED_QUALITY_VALUE = 0;
-    private static final int SELL_IN_VALUE_FIVE = 5;
-    private static final int SELL_IN_VALUE_TEN = 10;
-    private static final int QUALITY_VALUE_ONE = 1;
-    private static final int QUALITY_VALUE_TWO = 2;
-    private static final int QUALITY_VALUE_THREE = 3;
 
     public BackstagePassItemUpdater(Item item) {
         super(item);
@@ -14,26 +8,17 @@ class BackstagePassItemUpdater extends ItemUpdater {
 
     @Override
     void setQualityForValidItem() {
-        if (ifSellInIsLessThanOrEqualToTen()) {
-            increaseQualityBy(QUALITY_VALUE_TWO);
-        } else if
-        (ifSellInIsLessThanOrEqualToFive()) {
-            increaseQualityBy(QUALITY_VALUE_THREE);
+        if (isSellInLessThanOrEqualTo(5)) {
+            increaseQualityBy(3);
+        } else if (isSellInLessThanOrEqualTo(10)) {
+            increaseQualityBy(2);
         } else {
-            increaseQualityBy(QUALITY_VALUE_ONE);
+            increaseQualityBy(1);
         }
     }
 
     @Override
     void setQualityForExpiredItem() {
-        setQuality(EXPIRED_QUALITY_VALUE);
-    }
-
-    private boolean ifSellInIsLessThanOrEqualToTen() {
-        return isSellInWithin(SELL_IN_VALUE_FIVE, SELL_IN_VALUE_TEN) && isQualityBelowMax();
-    }
-
-    private boolean ifSellInIsLessThanOrEqualToFive() {
-        return isSellInLessThanOrEqualTo(SELL_IN_VALUE_FIVE) && isQualityBelowMax();
+        setQuality(0);
     }
 }
